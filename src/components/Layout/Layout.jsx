92f2d5f746028b232bd73c904d0bfed95181abc3
RebelM777 <molly666j@gmail.com>
2025-09-05 00:31:02 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./../Navbar/Navbar";
+import Navbar from "./../navbar/Navbar";
 import { Outlet } from "react-router";
 
 export default function Layout() {

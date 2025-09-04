542da286bb51180fa091d05260c11083823155ca
RebelM777 <molly666j@gmail.com>
2025-09-05 00:33:07 +0300

    Update Category.jsx

--- src/components/Shop/Category.jsx
+++ src/components/Shop/Category.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import ProductCard from "../Cards/ProductCard";
+import ProductCard from "../cards/ProductCard";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Category() {
